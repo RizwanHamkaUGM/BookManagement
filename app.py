35680d0f9938dff5f5aeb4e55e78c266686d9c4e
Rizwan Hamka <ahmadrizwanhamka@mail.ugm.ac.id>
2024-11-20 08:09:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 import requests
 from flask_cors import CORS
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -64,4 +65,5 @@ def delete_book(id):
     return response.json(), response.status_code
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host="0.0.0.0", port=port)
